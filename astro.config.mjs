966449e135607a4786a02afa3d631fead92f90da
zibgger <3420252006@qq.com>
2026-08-18 15:41:11 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://zibgger.github.io',
+  base: '/zibgger',
   server: {
     host: '0.0.0.0',
   },
